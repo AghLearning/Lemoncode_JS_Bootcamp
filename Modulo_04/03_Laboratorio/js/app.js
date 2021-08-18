2108566457d4ed4436c7c9499eeb4247dea4b819
Adolfo García <adolfogh@outlook.com>
2021-08-18 17:39:57 +0200

    Laboratorio Modulo 4

--- Modulo_04/03_Laboratorio/js/app.js
+++ Modulo_04/03_Laboratorio/js/app.js
@@ -1,4 +1,46 @@
-var sum = (a, b) => a + b; 
-var diff = (a,b) => a-b;
-var prod = (a,b) => a*b;
-var div = (a,b) => a/b;
+
+const validateData = (a, b) => (!isNaN(a) && !isNaN(b)) ? true : false;
+
+const printError = () => console.log("Error: Debe introducir dos valores numéricos.");
+const printResult = (result) => document.getElementById("txt-result").innerHTML = result;
+
+const sum = (a, b) => a + b;  
+const diff = (a,b) => a - b;  
+const prod = (a,b) => a * b;  
+const div = (a,b) =>  a / b; 
+
+console.log("Resultado Suma: " + sum(4,0));
+console.log("Resultado Resta: " + diff(4,0));
+console.log("Resultado Mult.: " + prod(4,2));
+console.log("Resultado Div.: " + div(4,2));
+
+const getValueA = () => parseInt(document.getElementById("input-a").value);
+const getValueB = () => parseInt(document.getElementById("input-b").value);
+
+
+const handleSum = () => {
+    validateData(getValueA(), getValueB()) ? printResult(sum(getValueA(), getValueB())) : printError();
+}
+
+const handleDiff = () => {
+    validateData(getValueA(), getValueB()) ? printResult(diff(getValueA(), getValueB())) : printError();
+}
+
+const handleProd = () => {
+    validateData(getValueA(), getValueB()) ? printResult(prod(getValueA(), getValueB())) : printError();
+}
+
+const handleDiv = () => {
+    validateData(getValueA(), getValueB()) && getValueB() !=0 ? printResult(div(getValueA(), getValueB())) : printError();
+}
+
+
+
+document.getElementById("btn-sum").addEventListener("click", handleSum);
+document.getElementById("btn-dif").addEventListener("click", handleDiff);
+document.getElementById("btn-prod").addEventListener("click", handleProd);
+document.getElementById("btn-div").addEventListener("click", handleDiv);
+// document.getElementById("btn-div").addEventListener("click", function() {
+//     validateData(getValueA(), getValueB()) && getValueB() !=0 ? printResult(div(getValueA(), getValueB())) : printError();
+// });
+
